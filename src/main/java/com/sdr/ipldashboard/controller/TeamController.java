fd8c687366536671ada06a3948919dc10b1762b7
sohamderoy <sohamderoy.iitk@gmail.com>
2022-02-23 01:15:15 +0530

    Added all files

---
+++ src/main/java/com/sdr/ipldashboard/controller/TeamController.java
@@ -0,0 +1,43 @@
+package com.sdr.ipldashboard.controller;
+
+import com.sdr.ipldashboard.model.Match;
+import com.sdr.ipldashboard.model.Team;
+import com.sdr.ipldashboard.repository.MatchRepository;
+import com.sdr.ipldashboard.repository.TeamRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class TeamController {
+
+    private TeamRepository teamRepository;
+    private MatchRepository matchRepository;
+
+    public TeamController(TeamRepository teamRepository, MatchRepository matchRepository) {
+        this.teamRepository = teamRepository;
+        this.matchRepository = matchRepository;
+    }
+
+    @GetMapping("/team/{teamName}")
+    public Team getTeam(@PathVariable String teamName){
+        Team team = this.teamRepository.findByTeamName(teamName);
+        team.setMatches(matchRepository.findLatestMatchesByTeam(teamName, 4));
+
+        return team;
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year){
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1,1);
+        return this.matchRepository.getMatchesByTeamBetweenDates(teamName, startDate, endDate);
+    }
+
+    @GetMapping("/team")
+    public Iterable<Team> getAllTeam(){
+        return this.teamRepository.findAll();
+    }
+}
